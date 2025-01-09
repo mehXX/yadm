a78b7280b31c1e2067edca6997ed0b4aa175423a
Nikita Naumov <n.naumov@uzum.com>
2025-01-09 12:06:28 +0500

    qwe

--- settings/bin/sync_config.sh
+++ settings/bin/sync_config.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+start_time=$(get_current_time)
+echo "START: yadm"
+
 #  /opt/homebrew/bin/git --git-dir="$HOME/.git_settings/" --work-tree="$HOME" commit -m "karabiner.json updated" && \
 #  /opt/homebrew/bin/git --git-dir="$HOME/.git_settings/" --work-tree="$HOME" push origin master
 /opt/homebrew/bin/yadm add -u
 /opt/homebrew/bin/yadm add "$HOME/settings"
 /opt/homebrew/bin/yadm commit -m "qwe"
 /opt/homebrew/bin/yadm push origin master
+
+log_duration_and_complete $start_time "END: yadm done"
