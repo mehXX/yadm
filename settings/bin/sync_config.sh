4eb45707c4ca59120cecf6807d42726d6e767771
Nikita Naumov <n.naumov@uzum.com>
2025-01-09 11:38:12 +0500

    qwe

--- settings/bin/sync_config.sh
+++ settings/bin/sync_config.sh
@@ -7,5 +7,7 @@ CHANGES=$(/opt/homebrew/bin/git --git-dir="$HOME/.git_settings/" --work-tree="$H
 if [ -n "$CHANGES" ]; then
 #  /opt/homebrew/bin/git --git-dir="$HOME/.git_settings/" --work-tree="$HOME" commit -m "karabiner.json updated" && \
 #  /opt/homebrew/bin/git --git-dir="$HOME/.git_settings/" --work-tree="$HOME" push origin master
-
+    /opt/homebrew/bin/yadm add "$HOME/settings"
+    /opt/homebrew/bin/yadm commit -m "qwe"
+    /opt/homebrew/bin/yadm push origin master
 fi
